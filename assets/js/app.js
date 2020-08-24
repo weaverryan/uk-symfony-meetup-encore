460c19aff2c103c07e2f9c5cd2946802f4ca0ae0
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:55 -0400

    Step 35: Async: Wrap in function

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,15 @@ import axios from 'axios';
 
 const storeEl = document.querySelector('#store');
 
-const response = await axios.get('/api/products');
-const products = response.data.products;
+function startStoreApp() {
+    const response = await axios.get('/api/products');
+    const products = response.data.products;
 
-const store = new Store(storeEl, products);
-store.initialize();
+    const store = new Store(storeEl, products);
+    store.initialize();
+}
+
+startStoreApp();
 
 const h1 = document.querySelector('h1');
 h1.innerHTML = `<img src="${sheep}" alt="baa" height="30">` + h1.innerHTML;
