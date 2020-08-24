061888aba5f8ee58ea6fa264cb1487b4e8e83050
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:59 -0400

    Step 49: Stimulus: move code into initialize()

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 import '../styles/styles.scss';
 import sheep from '../images/sheep.png';
-import axios from 'axios';
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
@@ -9,21 +8,6 @@ const application = Application.start()
 const context = require.context('./controllers', true, /\.js$/)
 application.load(definitionsFromContext(context))
 /* END STIMULUS SETUP */
-
-const storeEl = document.querySelector('#store');
-if (storeEl) {
-    import('./components/store').then(Store => {
-        async function startStoreApp() {
-            const response = await axios.get('/api/products');
-            const products = response.data.products;
-
-            const store = new Store.default(storeEl, products);
-            store.initialize();
-        }
-
-        startStoreApp();
-    });
-}
 
 const h1 = document.querySelector('h1');
 h1.innerHTML = `<img src="${sheep}" alt="baa" height="30">` + h1.innerHTML;
--- assets/js/controllers/store_controller.js
+++ assets/js/controllers/store_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from 'stimulus';
+import axios from 'axios';
+import Store from '../components/store';
 
 export default class extends Controller {
-    initialize() {
-        console.log(this.element);
+    async initialize() {
+        const response = await axios.get('/api/products');
+        const products = response.data.products;
+
+        const store = new Store(this.element, products);
+        store.initialize();
     }
 }
