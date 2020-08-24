6a116942c1a8a6dea255c6917572ceeb3801fe49
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:55 -0400

    Step 34: Async: try to use await

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,12 @@ import sheep from '../images/sheep.png';
 import axios from 'axios';
 
 const storeEl = document.querySelector('#store');
-axios.get('/api/products').then(response => {
-    const products = response.data.products;
 
-    const store = new Store(storeEl, products);
-    store.initialize();
-});
+const response = await axios.get('/api/products');
+const products = response.data.products;
+
+const store = new Store(storeEl, products);
+store.initialize();
 
 const h1 = document.querySelector('h1');
 h1.innerHTML = `<img src="${sheep}" alt="baa" height="30">` + h1.innerHTML;
