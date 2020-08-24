98a297f72423b2921f79ddd34102c3b4883088d1
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:52:00 -0400

    Step 51: Stimulus: show data-target

--- assets/js/controllers/store_controller.js
+++ assets/js/controllers/store_controller.js
@@ -2,13 +2,17 @@ import { Controller } from 'stimulus';
 import axios from 'axios';
 
 export default class extends Controller {
+    static get targets() {
+        return ['productList']
+    }
+
     async initialize() {
         const response = await axios.get('/api/products');
         this.products = response.data.products;
     }
 
     showProducts() {
-        this.element.querySelector('#store')
+        this.productListTarget
             .innerHTML = this.getProductsString();
     }
 
