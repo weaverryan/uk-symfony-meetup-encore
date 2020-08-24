76bf38831bc8e561b098dae7776c743e9b93ad32
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:56 -0400

    Step 38: MultiEntry: add store entry

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,5 @@
-import Store from './components/store';
 import '../styles/styles.scss';
 import sheep from '../images/sheep.png';
-import axios from 'axios';
-
-const storeEl = document.querySelector('#store');
-
-async function startStoreApp() {
-    const response = await axios.get('/api/products');
-    const products = response.data.products;
-
-    const store = new Store(storeEl, products);
-    store.initialize();
-}
-
-startStoreApp();
 
 const h1 = document.querySelector('h1');
 h1.innerHTML = `<img src="${sheep}" alt="baa" height="30">` + h1.innerHTML;
---
+++ assets/js/store.js
@@ -0,0 +1,14 @@
+import Store from './components/store';
+import axios from 'axios';
+
+const storeEl = document.querySelector('#store');
+
+async function startStoreApp() {
+    const response = await axios.get('/api/products');
+    const products = response.data.products;
+
+    const store = new Store(storeEl, products);
+    store.initialize();
+}
+
+startStoreApp();
