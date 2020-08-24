cfcf2209d9bf4ff7a857ed9a127a44972cb1a826
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:53 -0400

    Step 26: Image: import it

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 import Store from './store';
 import './styles.css';
+import sheep from './sheep.png';
 
 const storeEl = document.querySelector('#store');
 const store = new Store(storeEl, [
@@ -11,3 +12,6 @@ const store = new Store(storeEl, [
     'Shearly Conditioned'
 ]);
 store.initialize();
+
+const h1 = document.querySelector('h1');
+h1.innerHTML = `<img src="${sheep}" alt="baa" height="30">` + h1.innerHTML;
