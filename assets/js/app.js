106fa0bc2fa63d9726002f8982ed09e2089c967c
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:57 -0400

    Step 41: AsyncImport: Wrap logic in if

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,17 @@ import Store from './components/store';
 import axios from 'axios';
 
 const storeEl = document.querySelector('#store');
+if (storeEl) {
+    async function startStoreApp() {
+        const response = await axios.get('/api/products');
+        const products = response.data.products;
 
-async function startStoreApp() {
-    const response = await axios.get('/api/products');
-    const products = response.data.products;
+        const store = new Store(storeEl, products);
+        store.initialize();
+    }
 
-    const store = new Store(storeEl, products);
-    store.initialize();
+    startStoreApp();
 }
-
-startStoreApp();
 
 const h1 = document.querySelector('h1');
 h1.innerHTML = `<img src="${sheep}" alt="baa" height="30">` + h1.innerHTML;
