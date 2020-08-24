eb7a5f243a73057a8175a4417ff6528823c8d704
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 14:51:49 -0400

    Step 9: ES2015: class

--- public/app.js
+++ public/app.js
@@ -1,14 +1,24 @@
-const products = [
+class Store {
+    constructor(products) {
+        this.products = products;
+    }
+
+    getProductsString() {
+        return this.products.join(', ');
+    }
+}
+
+const store = new Store([
     'Sheer Shears',
     'Wool Hauling Basket',
     'After-Shear (Fresh Cut Grass)',
     'After-Shear (Morning Dew)',
     'Shear Comb',
     'Shearly Conditioned'
-];
+]);
 
 const button = document.querySelector('#button');
 const storeEl = document.querySelector('#store');
 button.addEventListener('click', function() {
-    storeEl.innerHTML = products.join(', ');
+    storeEl.innerHTML = store.getProductsString();
 });
